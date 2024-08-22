15bfbbfb6fc31c8c8b9caf16db7774e8fa5349fb
Svitlana Pavlyuk <119800111+PavlyukSvitlana@users.noreply.github.com>
2024-08-22 07:25:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,3 +19,50 @@ burgerBtn.addEventListener('click', (e) => {
 	toogleClass(burgerBtn, 'burger-btn__active');
 
 });
+
+
+
+//Data base Mock Api
+
+const db = 'https://66c6b3cd8b2c10445bc76b53.mockapi.io/api/v1/subscribe',
+	subscribeForm = document.querySelector('.sendEmail');
+
+subscribeForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const email = document.getElementById('email');
+
+	const data = {
+		email: email.value
+	};
+
+	fetch(db, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8'
+		},
+		body: JSON.stringify(data)
+	})
+});
+
+
+
+
+function getAndDisplayData() {
+	fetch(db)
+		.then(response => response.json())
+		.then(data => {
+
+			const list = document.querySelector('.subscribers__list');
+			list.innerHTML = '';
+
+			data.forEach(item => {
+				const listItem = document.createElement('li');
+				listItem.classList.add('subscribers__list-item');
+				listItem.textContent = `${item.id} ---> ${item.email}`;
+				list.appendChild(listItem);
+			});
+		})
+}
+
+document.addEventListener('DOMContentLoaded', getAndDisplayData);
